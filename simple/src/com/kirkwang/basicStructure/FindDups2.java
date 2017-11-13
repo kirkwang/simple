3a0bf402c43c7535809dcc519456c9e748c64903
kirk wang <kirkwangke@gmail.com>
2017-11-12 22:00:35 -0800

    add some comments

--- simple/src/com/kirkwang/basicStructure/FindDups2.java
+++ simple/src/com/kirkwang/basicStructure/FindDups2.java
@@ -7,15 +7,21 @@ import java.util.Set;
  * Created by amp on 9/21/2015.
  */
 public class FindDups2 {
-  public static void main (String[] args){
-    Set<String> uniques = new HashSet<String>();
-    Set<String> dups = new HashSet<String>();
+    public static void main(String[] args) {
+        String[] stringSet = {"aa", "bb", "cc", "dd", "bb", "aa"};
+        Set<String> uniques = new HashSet<String>();
 
-    for (String a : args){
-      if (!uniques.add(a)){
-        dups.add(a);
-      }
-      uniques.removeAll(dups);
+        Set<String> dups = new HashSet<String>();
+
+        for (String a : stringSet) {
+            if (!uniques.add(a)) {
+                dups.add(a);
+            }
+        }
+        uniques.removeAll(dups); // this method will remove the all the element from unique that are exist in dups
+        //    uniques.retainAll(dups);  //this method will keep the common element that in both set
+
+        System.out.println(uniques);
+        System.out.println(dups);
     }
-  }
 }
