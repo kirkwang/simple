fc24eba19aa2e77fae5067f559f4f34ea372cc72
kirk wang <kirkwangke@gmail.com>
2017-11-13 12:43:16 -0800

    adding a method to FindDups2

--- simple/src/com/kirkwang/basicStructure/FindDups2.java
+++ simple/src/com/kirkwang/basicStructure/FindDups2.java
@@ -1,21 +1,39 @@
 package com.kirkwang.basicStructure;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
  * Created by amp on 9/21/2015.
  */
 public class FindDups2 {
+
+    public static void printUniqeWords(List strings) {
+        if (strings == null) return;
+        Set uniqe = new HashSet<>();
+        for (Object a : strings) {
+            if ((uniqe.add(a))) {
+                System.out.println("This is unique string " + a);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String[] stringSet = {"aa", "bb", "cc", "dd", "bb", "aa"};
         Set<String> uniques = new HashSet<String>();
 
         Set<String> dups = new HashSet<String>();
 
+        printUniqeWords(Arrays.asList(stringSet));
+        printUniqeWords(null);
+
         for (String a : stringSet) {
             if (!uniques.add(a)) {
                 dups.add(a);
+                System.out.println(a);
+
             }
         }
         uniques.removeAll(dups); // this method will remove the all the element from unique that are exist in dups
