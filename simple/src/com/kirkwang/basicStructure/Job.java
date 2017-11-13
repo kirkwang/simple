2c98eb76c672407af189dc3cb1dc7d281b059fa9
kirk wang <kirkwangke@gmail.com>
2017-11-12 22:17:31 -0800

    add some comments

--- simple/src/com/kirkwang/basicStructure/Job.java
+++ simple/src/com/kirkwang/basicStructure/Job.java
@@ -4,28 +4,36 @@ package com.kirkwang.basicStructure;
  * Created by amp on 9/22/2015.
  */
 public class Job extends Thread {
-  private int counter;
+    private int counter;
 
-  @Override
-  public void run(){
-    synchronized (this){
-      for(int i =0; i <10000; i++){
-        counter++;
-      }
-      this.notifyAll();
-      System.out.println("completed counting....");
+    public static void main(String... args) throws InterruptedException {
+        Job job = new Job();
+        job.start();
+        Thread.sleep(1000);
+        System.out.println("waiting to get end....");
+        synchronized (job) {
+
+            System.out.println("about to sleep ");
+
+            if (!job.isAlive()) {
+                System.out.println("I am not alive");
+            }
+            //  job.wait(10000);
+
+
+        }
+
+        System.out.println(job.counter);
     }
-  }
 
-  public static void main(String... args) throws InterruptedException {
-    Job job= new Job();
-    job.start();
-    Thread.sleep(10000);
-    System.out.println("waiting to get end....");
-    synchronized (job){
-      job.wait();
+    @Override
+    public void run() {
+        synchronized (this) {
+            for (int i = 0; i < 1000; i++) {
+                counter++;
+            }
+            this.notifyAll();
+            System.out.println("completed counting....");
+        }
     }
-
-    System.out.println(job.counter);
-  }
 }
