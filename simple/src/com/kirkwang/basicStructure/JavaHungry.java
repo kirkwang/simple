07210eeefa878e261d733df3f459422cbd894b5b
kirk wang <kirkwangke@gmail.com>
2017-11-12 22:07:26 -0800

    add some comments

--- simple/src/com/kirkwang/basicStructure/JavaHungry.java
+++ simple/src/com/kirkwang/basicStructure/JavaHungry.java
@@ -1,28 +1,33 @@
 package com.kirkwang.basicStructure;
 
-import java.lang.System;
-
 /**
  * Created by amp on 9/22/2015.
  */
 public class JavaHungry {
-  public void m1(String m1){
-    m1 = "Am I going to fly?";
-  }
-  public static void main (String... args){
+    public static void main(String... args) {
 
-    JavaHungry test = new JavaHungry();
-    String iAmOfAnArgument = "I am born new";
-    test.m1(iAmOfAnArgument);
-    System.out.println(iAmOfAnArgument);
-    short s=0;
-    int x = 07;
-    int y = 8;
-    int z= 12123;
-    s+=z;
-    System.out.println(""+x+y+s);
-    if (x==2){
+        JavaHungry test = new JavaHungry();
+        String iAmOfAnArgument = "I am born new";
+        test.m1(iAmOfAnArgument);
+        System.out.println(iAmOfAnArgument);
+        short s = 1;
+        int x = 00007;
+        int y = 8;
+        int z = 12123;
+        s += z;
+        System.out.println("" + x + y + s);
+        if (x == 2) {
 
+        }
     }
-  }
+
+    /*
+    output
+    I am born new
+    7812124
+
+     */
+    public void m1(String m1) {
+        m1 = "Am I going to fly?";
+    }
 }
