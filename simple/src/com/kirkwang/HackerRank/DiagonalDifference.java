6682006d714fd4ba4426ee12c97e84b55d6005c6
kirk wang <kirkwangke@gmail.com>
2017-11-14 22:29:27 -0800

    change code style

--- simple/src/com/kirkwang/HackerRank/DiagonalDifference.java
+++ simple/src/com/kirkwang/HackerRank/DiagonalDifference.java
@@ -4,14 +4,15 @@ package com.kirkwang.HackerRank;
  * Created by kewang on 11/14/17.
  */
 public class DiagonalDifference {
-    public static int diagonalDifference(int[] input) {
-        return 0;
-    }
 
-    public static void main(String[] args) {
-        int[] array = {1, 1, 2, 4, 5};
-        Integer me = DiagonalDifference.diagonalDifference(array);
-        System.out.print(me);
-    }
+  public static int diagonalDifference(int[] input) {
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    int[] array = {1, 1, 2, 4, 5};
+    Integer me = DiagonalDifference.diagonalDifference(array);
+    System.out.print(me);
+  }
 
 }
