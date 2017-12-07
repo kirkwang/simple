31c3fdc5ce69a79cde0ee0b9cf6e78a5cab1be43
kirk wang <kirkwangke@gmail.com>
2017-12-06 22:04:09 -0800

    adding ContainsNearByDub.java

--- simple/src/com/kirkwang/easy/ContainsNearByDub.java
+++ simple/src/com/kirkwang/easy/ContainsNearByDub.java
@@ -6,23 +6,30 @@ import java.util.HashMap;
  * Created by kewang on 3/3/16.
  */
 public class ContainsNearByDub {
-    public static void main(String[] args) {
-        int[] numbs = {1, 4, 5, 1, 56, 22, 4};
-        System.out.println(ContainsNearByDub(numbs, 3));
-    }
 
-    public static boolean ContainsNearByDub(int[] numbs, int k) {
-        HashMap<Integer, Integer> map = new HashMap();
-        for (int i = 0; i < numbs.length; i++) {
-            if (map.containsKey(numbs[i])) {
-                int prevLoc = map.get(numbs[i]);
-                if (i - prevLoc <= k)
-                    return true;
-            }
-            map.put(numbs[i], i);
+  /*
+  Given an array of integers, find if the array contains any duplicates.
+  Your function should return true if any value appears at least twice in the array,
+   and it should return false if every element is distinct.
+   */
+  public static void main(String[] args) {
+    int[] numbs = {1, 4, 5, 1, 56, 22, 4};
+    System.out.println(ContainsNearByDub(numbs, 3));
+  }
+
+  public static boolean ContainsNearByDub(int[] numbs, int k) {
+    HashMap<Integer, Integer> map = new HashMap();
+    for (int i = 0; i < numbs.length; i++) {
+      if (map.containsKey(numbs[i])) {
+        int prevLoc = map.get(numbs[i]);
+        if (i - prevLoc <= k) {
+          return true;
         }
-
-        return false;
+      }
+      map.put(numbs[i], i);
     }
+
+    return false;
+  }
 
 }
