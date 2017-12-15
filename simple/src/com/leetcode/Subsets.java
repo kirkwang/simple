3c638d90ea1dc7ed1718853e27cc884661c958d0
kirk wang <kirkwangke@gmail.com>
2017-12-15 11:56:17 -0800

    format update

---
+++ simple/src/com/leetcode/Subsets.java
@@ -0,0 +1,20 @@
+package com.leetcode;
+
+/**
+ * Created by kewang on 12/15/17.
+ *
+ *
+ * https://leetcode.com/problems/subsets/description/
+ */
+public class Subsets {
+
+  public static int subsets(int input) {
+    return input;
+  }
+
+  public static void main(String[] args) {
+    int result = subsets(5);
+    System.out.println("This is the result " + result);
+
+  }
+}
