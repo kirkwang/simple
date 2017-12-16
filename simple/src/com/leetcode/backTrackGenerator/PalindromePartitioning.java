1e3a237d0bae09f91caa38d64af07a664b4eca22
kirk wang <kirkwangke@gmail.com>
2017-12-16 15:04:57 -0800

    backTrackGenerator package

---
+++ simple/src/com/leetcode/backTrackGenerator/PalindromePartitioning.java
@@ -0,0 +1,30 @@
+package com.leetcode.backTrackGenerator;
+
+/**
+ * Created by kewang on 12/16/17.
+ *
+ * https://leetcode.com/problems/palindrome-partitioning/description/
+ *
+ * Given a string s, partition s such that every substring of the partition is a palindrome.
+ *
+ * Return all possible palindrome partitioning of s.
+ *
+ * For example, given s = "aab", Return
+ *
+ * [ ["aa","b"], ["a","a","b"] ]
+ */
+public class PalindromePartitioning {
+
+  public static String PalindromePartitioning() {
+    return null;
+  }
+
+  public static void main(String[] args) {
+    int target = 7;
+    System.out.println("This is the result " + target);
+
+    System.out.println("This is the result " + PalindromePartitioning());
+
+  }
+
+}
--- simple/src/com/leetcode/backTrackGenerator/Permutations_I.java
+++ simple/src/com/leetcode/backTrackGenerator/Permutations_I.java
@@ -1,4 +1,4 @@
-package com.leetcode;
+package com.leetcode.backTrackGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
