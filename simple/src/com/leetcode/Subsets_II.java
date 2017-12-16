6869bf9a9fc2642fb8a84340783ee4f6a642717f
kirk wang <kirkwangke@gmail.com>
2017-12-15 23:05:21 -0800

    Subsets

--- simple/src/com/leetcode/Subsets_II.java
+++ simple/src/com/leetcode/Subsets_II.java
@@ -8,25 +8,26 @@ import java.util.List;
  * Created by kewang on 12/15/17.
  *
  *
- * https://leetcode.com/problems/subsets/description/
+ * https://leetcode.com/problems/subsets-ii/description/
+ * https://leetcode.com/problems/subsets/discuss/
  *
  *
- * Given a set of distinct integers, nums, return all possible subsets (the power set).
+ * Given a collection of integers that might contain duplicates, nums, return all possible subsets
+ * (the power set).
  *
  * Note: The solution set must not contain duplicate subsets.
  *
- * For example, If nums = [1,2,3], a solution is:
+ * For example, If nums = [1,2,2], a solution is:
  *
- * [ [3], [1], [2], [1,2,3], [1,3], [2,3], [1,2], [] ]
+ * [ [2], [1], [1,2,2], [2,2], [1,2], [] ]
  */
 public class Subsets_II {
 
   public static void main(String[] args) {
-    List result = subsets(new int[]{1, 2, 3});
+    List result = subsets(new int[]{1, 2, 2});
     System.out.println("This is the result " + result);
   }
 
-  // public static int subsets(int input) {
   public static List<List<Integer>> subsets(int[] nums) {
     List<List<Integer>> list = new ArrayList<>();
     if (nums == null) {
@@ -44,6 +45,10 @@ public class Subsets_II {
     list.add(new ArrayList<>(objects));
 
     for (int i = j; i < nums.length; i++) {
+      //if loop starts and current item is same as previous , skip
+      if (i > j && nums[i] == nums[i - 1]) {
+        continue;
+      }
       objects.add(nums[i]);
       generateSubSet(list, objects, nums, i + 1);
       objects.remove(objects.size() - 1);
