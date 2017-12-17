6f1c7019ab536ee2eb089b028572140b1d81f5f8
kirk wang <kirkwangke@gmail.com>
2017-12-16 23:32:58 -0800

    refactor AbsDistinct

--- simple/src/com/kirkwang/Codility/AbsDistinct.java
+++ simple/src/com/kirkwang/Codility/AbsDistinct.java
@@ -1,8 +1,13 @@
 package com.kirkwang.Codility;
 
 /**
- * Created by kewang on 11/16/17. For example, consider array A such that: A[0] = -5 A[1] = -3 A[2]
- * = -1 A[3] =  0 A[4] =  3 A[5] =  6
+ * Created by kewang on 11/16/17.
+ *
+ *
+ * http://www.geeksforgeeks.org/absolute-distinct-count-array-sorted-absolute-values/
+ *
+ * For example, consider array A such that: A[0] = -5 A[1] = -3 A[2] = -1 A[3] =  0 A[4] =  3 A[5] =
+ * 6
  *
  * The absolute distinct count of this array is 5, because there are 5 distinct absolute values
  * among the elements of this array, namely 0, 1, 3, 5 and 6.
@@ -10,10 +15,15 @@ package com.kirkwang.Codility;
 public class AbsDistinct {
 
   public static int AbsDistinct(int[] A) {
-
+    if (A == null || A.length == 0) {
+      return 0;
+    }
     java.util.Set set = new java.util.HashSet<>();
     for (int i = 0; i < A.length; i++) {
-      set.add(Math.abs(A[i]));
+      if (A[i] < 0) {
+        A[i] *= -1;
+      }
+      set.add(A[i]);
     }
 
     return set.size();
