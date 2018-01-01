8c7b5b7414b8411ac9ad35c71af6e5ba4761021c
kirk wang <kirkwangke@gmail.com>
2018-01-01 11:43:48 -0800

    bulk upload

--- simple/src/com/kirkwang/basicStructure/FindDups2.java
+++ simple/src/com/kirkwang/basicStructure/FindDups2.java
@@ -23,24 +23,24 @@ public class FindDups2 {
   }
 
   public static void main(String[] args) {
-    String[] stringSet = {"aa", "bb", "cc", "dd", "bb", "aa"};
+    String[] strings = {"aa", "bb", "cc", "dd", "bb", "aa"};
     Set<String> uniques = new HashSet<String>();
 
     Set<String> dups = new HashSet<>();
 
-    printUniqeWords(Arrays.asList(stringSet));
+    printUniqeWords(Arrays.asList(strings));
     printUniqeWords(null);
 
-    for (String a : stringSet) {
+    for (String a : strings) {
       if (!uniques.add(a)) {
         dups.add(a);
         System.out.println(a);
 
       }
     }
-    uniques.removeAll(
-        dups); // this method will remove the all the element from unique that are exist in dups
-    //    uniques.retainAll(dups);  //this method will keep the common element that in both set
+    uniques
+        .removeAll(dups); // this will remove the all the element from unique that are exist in dups
+    uniques.retainAll(dups);  //this method will keep the common element that in both set
 
     System.out.println(uniques);
     System.out.println(dups);
---
+++ simple/src/com/leetcode/WordBreak.java
@@ -0,0 +1,34 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kewang on 12/25/17.
+ *
+ * https://leetcode.com/problems/word-break/description/
+ *
+ *
+ * Given a string s and a dictionary of words dict, determine if s can be segmented into a
+ * space-separated sequence of one or more dictionary words. For example, given s = "leetcode", dict
+ * = ["leet", "code"]. Return true because "leetcode" can be segmented as "leet code".
+ *
+ * The answer is here https://www.jianshu.com/p/d88a7e49987d
+ */
+public class WordBreak {
+
+  public static boolean WordBreak(String s, List<String> wordDict) {
+
+    return false;
+  }
+
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<>();
+    list.add("leet");
+    list.add("code");
+    String s = "leetCode";
+    boolean result = WordBreak(s, list);
+    System.out.print(result);
+  }
+
+}
