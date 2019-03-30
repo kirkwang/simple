2092e79e49a3e9307d57281bd7e27699bc2461e7
kirkwang <kirkwangke@gmail.com>
2019-03-29 22:45:11 -0700

    Capture.java

--- simple/src/com/kirkwang/easy/Capture.java
+++ simple/src/com/kirkwang/easy/Capture.java
@@ -1,10 +1,6 @@
 package com.kirkwang.easy;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by amp on 9/21/2015.
