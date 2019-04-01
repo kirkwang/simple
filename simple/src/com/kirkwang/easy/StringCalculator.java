c6da0b57c86b2fd74d799827baef07450b17c3b0
kirkwang <kirkwangke@gmail.com>
2019-03-31 19:43:36 -0700

    StringCalculator  + - only

---
+++ simple/src/com/kirkwang/easy/StringCalculator.java
@@ -0,0 +1,45 @@
+/*
+ * 3/31/19 3:22 PM : for the current date and time;
+ * 2019 : to access the year information of the current date;
+ * kirkwang : the currently used username;
+ */
+
+package com.kirkwang.easy;
+
+class StringCalculator {
+
+    public static void main(String[] args) {
+        String input = "-5+2-4";
+        StringCalculator sc = new StringCalculator();
+        sc.StringCalculator(input);
+    }
+
+    int StringCalculator(String input) {
+        String sign = null;
+        if (!Character.isDigit(input.charAt(0))) {
+            sign = String.valueOf(input.charAt(0));
+            input = input.substring(1);
+        } else {
+            sign = "+";
+        }
+
+        String[] operator = input.split("[0-9]+");
+        String[] operands = input.split("[+-]");
+
+        int aggrateor = Integer.parseInt(operands[0]);
+
+        for (int i = 1; i < operands.length; i++) {
+            if (operator[i].equals("+")) {
+                aggrateor += Integer.parseInt(operands[i]);
+            } else {
+                aggrateor -= Integer.parseInt(operands[i]);
+            }
+        }
+
+        if (sign.equals("-")) {
+            aggrateor -= 2 * Integer.valueOf(operands[0]);
+        }
+        System.out.println("aggrateor " + aggrateor);
+        return aggrateor;
+    }
+}
