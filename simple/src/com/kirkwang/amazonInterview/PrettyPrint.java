8fa23247a94a2e91d4877e2678ae72b85cf03b59
kirkwang <kirkwangke@gmail.com>
2019-04-03 09:20:31 -0700

    Amazon Seattle is a nice place. Work hard. Have fun. Make history.

---
+++ simple/src/com/kirkwang/amazonInterview/PrettyPrint.java
@@ -0,0 +1,49 @@
+/*
+ * 4/2/19 4:41 PM : for the current date and time;
+ * 2019 : to access the year information of the current date;
+ * kirkwang : the currently used username;
+ */
+
+package com.kirkwang.amazonInterview;
+
+public class PrettyPrint {
+    public static void main(String[] args) {
+        String input = "Sea!tle is a nice place. Work Hard! have Fun, make HIStory!";
+        new PrettyPrint().prettyPrint(input);
+    }
+
+    public void prettyPrint(String input) {
+        String[] spaceSplit = input.split(" ");
+        for (int i = 0; i < spaceSplit.length; i++) {
+            String word = spaceSplit[i];
+            int lastIndex = word.length() - 1;
+            if (word.charAt(lastIndex) == '!' || word.charAt(lastIndex) == ',' || word.charAt(lastIndex) == '.') {
+                StringBuilder sb = new StringBuilder();
+                sb.append(word, 0, lastIndex);
+                sb.append(".");
+                spaceSplit[i] = sb.toString().toLowerCase();
+            }
+        }
+        StringBuilder sentence = new StringBuilder();
+        for (int i = 0; i < spaceSplit.length; i++) {
+
+            char lastChar = spaceSplit[i].charAt(spaceSplit[i].length() - 1);
+
+            if (lastChar != '.') {
+                sentence = sentence.append(spaceSplit[i]).append(" ");
+                String firstChar = String.valueOf(sentence.charAt(0));
+                sentence.setCharAt(0, firstChar.toUpperCase().toCharArray()[0]);
+
+            } else {
+                sentence = sentence.append(spaceSplit[i]).append(" ");
+                String firstChar = String.valueOf(sentence.charAt(0));
+                sentence.setCharAt(0, firstChar.toUpperCase().toCharArray()[0]);
+                System.out.println(sentence.toString().replace('!', 't'));
+                sentence = new StringBuilder();
+            }
+
+
+        }
+    }
+
+}
