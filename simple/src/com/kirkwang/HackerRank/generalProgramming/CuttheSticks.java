2e9984c557c48b33e4da4457874d279932562a24
kirkwang <kirkwangke@gmail.com>
2019-04-24 07:38:21 -0700

    cuttheSticks

--- simple/src/com/kirkwang/HackerRank/generalProgramming/CuttheSticks.java
+++ simple/src/com/kirkwang/HackerRank/generalProgramming/CuttheSticks.java
@@ -6,15 +6,31 @@
 
 package com.kirkwang.HackerRank.generalProgramming;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CuttheSticks {
     public static void main(String[] args) {
 
-        int[] k = {1, 1, 3, 4, 5, 6, 8};
+        int[] k = {5, 4, 4, 2, 2, 8};
+        int[] l = {1, 2, 3, 4, 3, 3, 2, 1};
 
         System.out.println(cuttheSticks(k));
     }
 
     public static int[] cuttheSticks(int[] arr) {
-        return arr;
+        Arrays.sort(arr);
+        List<Integer> arrayList = new ArrayList<Integer>();
+        System.out.println("array length " + arr.length);
+        arrayList.add(arr.length);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                System.out.println(arr.length - i);
+                arrayList.add(arr.length - i);
+            }
+        }
+        return arrayList.stream().mapToInt(i -> i).toArray();
+
     }
 }
