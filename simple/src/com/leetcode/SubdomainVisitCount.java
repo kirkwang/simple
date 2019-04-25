19b084c57ce09b139c681d515ea17a2848ba684b
kirkwang <kirkwangke@gmail.com>
2019-04-25 12:09:33 -0700

    SubdomainVisitCount

--- simple/src/com/leetcode/SubdomainVisitCount.java
+++ simple/src/com/leetcode/SubdomainVisitCount.java
@@ -7,6 +7,7 @@
 package com.leetcode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class SubdomainVisitCount {
@@ -14,11 +15,27 @@ public class SubdomainVisitCount {
 
         String[] input = new String[]{"900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"};
 
-
         System.out.println(subdomainVisits(input));
     }
 
     public static List<String> subdomainVisits(String[] cpdomains) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < cpdomains.length; i++) {
+            String[] domainSplit = cpdomains[i].split(" ");
+            int count = Integer.valueOf(domainSplit[0]);
+            String domain = domainSplit[1];
+
+            int index = 0;
+            while (true) {
+                String subdomain = domain.substring(index); //something.domain.com has index 0.1.2 as on line 32
+                hashMap.put(subdomain, hashMap.getOrDefault(subdomain, 0) + count);
+                index = domain.indexOf(".", index); //index has begin index or start index
+                if (index++ < 0) {
+                    break;
+                }
+                // index++;
+            }
+        }
 
         return new ArrayList<>();
     }
