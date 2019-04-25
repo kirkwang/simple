45a28b2aad7f51c439e50f9f0283b43dde4b6feb
kirkwang <kirkwangke@gmail.com>
2019-04-25 10:40:58 -0700

    subdomainVisitCount

---
+++ simple/src/com/leetcode/SubdomainVisitCount.java
@@ -0,0 +1,25 @@
+/*
+ * 4/25/19 10:37 AM : for the current date and time;
+ * 2019 : to access the year information of the current date;
+ * kirkwang : the currently used username;
+ */
+
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubdomainVisitCount {
+    public static void main(String[] args) {
+
+        String[] input = new String[]{"900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"};
+
+
+        System.out.println(subdomainVisits(input));
+    }
+
+    public static List<String> subdomainVisits(String[] cpdomains) {
+
+        return new ArrayList<>();
+    }
+}
