1b5649f5fe2b403c5256b966f230588ffe43d11c
kirkwang <kirkwangke@gmail.com>
2019-04-26 13:29:21 -0700

    CaesarCipher no rotation

--- simple/src/com/kirkwang/HackerRank/generalProgramming/CaesarCipher.java
+++ simple/src/com/kirkwang/HackerRank/generalProgramming/CaesarCipher.java
@@ -6,15 +6,31 @@
 
 package com.kirkwang.HackerRank.generalProgramming;
 
+
 public class CaesarCipher {
 
     static String caesarCipher(String s, int k) {
+        StringBuilder result = new StringBuilder();
 
-        return s;
+        for (char c : s.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+
+                char newChar = (char) ((c + k));
+                result.append(newChar);
+            } else if (Character.isLowerCase(c)) {
+                char newChar = (char) ((c + k));
+                result.append(newChar);
+            } else {
+
+                result.append(c);
+            }
+        }
+
+        return result.toString();
     }
 
     public static void main(String[] args) {
-        String beabeefeab = "beabeefeab";
+        String beabeefeab = "Aeabeefeab";
         int shift = 3;
         String ml = caesarCipher(beabeefeab, shift);
         System.out.println(ml);
