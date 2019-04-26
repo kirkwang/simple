8ddb663f0f7c3a785ad8e0b88db9e88f1e2146dc
kirkwang <kirkwangke@gmail.com>
2019-04-25 22:25:18 -0700

    CommonChild

---
+++ simple/src/com/kirkwang/HackerRank/generalProgramming/CamelCase.java
@@ -0,0 +1,10 @@
+/*
+ * 4/25/19 10:22 PM : for the current date and time;
+ * 2019 : to access the year information of the current date;
+ * kirkwang : the currently used username;
+ */
+
+package com.kirkwang.HackerRank.generalProgramming;
+
+public class CamelCase {
+}
---
+++ simple/src/com/kirkwang/HackerRank/generalProgramming/CommonChild.java
@@ -0,0 +1,38 @@
+/*
+ * 4/25/19 9:49 PM : for the current date and time;
+ * 2019 : to access the year information of the current date;
+ * kirkwang : the currently used username;
+ */
+
+package com.kirkwang.HackerRank.generalProgramming;
+
+import java.util.Scanner;
+
+public class CommonChild {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String a = in.next();
+        String b = in.next();
+        System.out.println(findMaxCommonLength(a, b));
+
+        in.close();
+    }
+
+    static int findMaxCommonLength(String s1, String s2) {
+        int length1 = s1.length();
+        int length2 = s2.length();
+        int[][] commonLengths = new int[length1 + 1][length2 + 1];
+        for (int i = 1; i <= length1; i++) {
+            for (int j = 1; j <= length2; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    commonLengths[i][j] = commonLengths[i - 1][j - 1] + 1;
+                } else {
+                    commonLengths[i][j] = Math.max(commonLengths[i - 1][j], commonLengths[i][j - 1]);
+                }
+            }
+        }
+        return commonLengths[length1][length2];
+    }
+
+}
