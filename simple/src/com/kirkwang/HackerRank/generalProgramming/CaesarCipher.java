1815c516c29fa738410c93a54d6f9eeb079d8c65
kirkwang <kirkwangke@gmail.com>
2019-04-26 10:38:26 -0700

    CaesarCipher

---
+++ simple/src/com/kirkwang/HackerRank/generalProgramming/CaesarCipher.java
@@ -0,0 +1,23 @@
+/*
+ * 4/26/19 10:36 AM : for the current date and time;
+ * 2019 : to access the year information of the current date;
+ * kirkwang : the currently used username;
+ */
+
+package com.kirkwang.HackerRank.generalProgramming;
+
+public class CaesarCipher {
+
+    static String caesarCipher(String s, int k) {
+
+        return s;
+    }
+
+    public static void main(String[] args) {
+        String beabeefeab = "beabeefeab";
+        int shift = 3;
+        String ml = caesarCipher(beabeefeab, shift);
+        System.out.println(ml);
+    }
+
+}
---
+++ simple/src/com/kirkwang/HackerRank/generalProgramming/TwoCharacters.java
@@ -0,0 +1,45 @@
+/*
+ * 4/26/19 9:50 AM : for the current date and time;
+ * 2019 : to access the year information of the current date;
+ * kirkwang : the currently used username;
+ */
+
+package com.kirkwang.HackerRank.generalProgramming;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+public class TwoCharacters {
+    static int alternate(String s) {
+        Set<Integer> letters = s.chars().collect(HashSet::new, Set::add, Set::addAll);
+        int maxLength = 0;
+        for (int letter1 : letters) {
+            for (int letter2 : letters) {
+//                if (letter2 == letter1) {
+//                    continue;
+//                }
+
+                List<Integer> remains = s.chars().filter(ch -> ch == letter1 || ch == letter2).collect(ArrayList::new,
+                        List::add, List::addAll);
+
+                if (IntStream.range(0, remains.size())
+                        .allMatch(i -> remains.get(i) == (i % 2 == 0 ? letter1 : letter2))) {
+                    maxLength = Math.max(maxLength, remains.size());
+                }
+            }
+        }
+        return maxLength;
+
+    }
+
+    public static void main(String[] args) {
+        String beabeefeab = "beabeefeab";
+
+        int ml = alternate(beabeefeab);
+        System.out.println(ml);
+    }
+
+}
