0ca3eb70a8c401f7bbf0742e9bc2c5a96a313dcb
kirkwang <kirkwangke@gmail.com>
2019-04-26 15:03:32 -0700

    marsExploration

--- simple/src/com/kirkwang/HackerRank/generalProgramming/MarsExploration.java
+++ simple/src/com/kirkwang/HackerRank/generalProgramming/MarsExploration.java
@@ -6,15 +6,28 @@
 
 package com.kirkwang.HackerRank.generalProgramming;
 
+import java.util.stream.IntStream;
+
 public class MarsExploration {
 
     static int marsExploration(String s) {
         int result = 0;
+        for (int i = 0, j = i + 3; j <= s.length(); ) {
+            result = result + countSos(s.substring(i, j));
+            i = i + 3;
+            j = j + 3;
+        }
         return result;
     }
 
+    static int countSos(String input) {
+
+        String UNIT = "SOS";
+        String part = input;
+        return (int) IntStream.range(0, 3).filter(i -> input.charAt(i) != UNIT.charAt(i)).count();
+    }
     public static void main(String[] args) {
-        String beabeefeab = "Jyabeefeab";
+        String beabeefeab = "SOSSPSSQSSOR";
         int shift = 3;
         shift = marsExploration(beabeefeab);
         System.out.println(shift);
