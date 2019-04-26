c099cfc719d292550adda3985a0a972e415320f6
kirkwang <kirkwangke@gmail.com>
2019-04-25 22:38:21 -0700

    CamelCase

--- simple/src/com/kirkwang/HackerRank/generalProgramming/CamelCase.java
+++ simple/src/com/kirkwang/HackerRank/generalProgramming/CamelCase.java
@@ -7,4 +7,25 @@
 package com.kirkwang.HackerRank.generalProgramming;
 
 public class CamelCase {
+    public static void main(String[] args) {
+
+        String a = "saveChangesInTheEditor";
+        System.out.println(camelcaseJ8(a));
+    }
+
+    static int camelcase(String s) {
+        int wordCount = 1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+                wordCount++;
+            }
+        }
+        return wordCount;
+    }
+
+    static int camelcaseJ8(String a) {
+
+        int count = (int) a.chars().filter(Character::isUpperCase).count();
+        return count;
+    }
 }
