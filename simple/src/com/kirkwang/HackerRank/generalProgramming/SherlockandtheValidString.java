956db6d5a119b3c649ccc447382792bd54a2b74b
kirkwang <kirkwangke@gmail.com>
2019-04-28 15:40:24 -0700

    SherlockandtheValidString

---
+++ simple/src/com/kirkwang/HackerRank/generalProgramming/SherlockandtheValidString.java
@@ -0,0 +1,48 @@
+/*
+ * 4/28/19 2:19 PM : for the current date and time;
+ * 2019 : to access the year information of the current date;
+ * kirkwang : the currently used username;
+ */
+
+package com.kirkwang.HackerRank.generalProgramming;
+
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class SherlockandtheValidString {
+    public static void main(String[] args) {
+
+        String s = "abcdefghhgfedecba";
+        String s1 = "aabbcd";
+
+
+        String result = isValid(s1);
+        System.out.println(result);
+
+    }
+
+    // Wrong answer
+    static String isValid(String s) {
+        HashMap<Character, Integer> hashMap = new HashMap();
+        for (Character c : s.toCharArray()) {
+            if (hashMap.containsKey(c)) {
+                hashMap.put(c, hashMap.get(c) + 1);
+            } else {
+                hashMap.put(c, 1);
+            }
+        }
+
+        TreeSet<Integer> ts = new TreeSet();
+        for (Integer i : hashMap.values()) {
+            ts.add(i);
+        }
+        if (ts.size() <= 2) {
+            if (Math.abs(ts.first() - ts.last()) == 1) {
+                return "YES";
+            }
+
+        }
+        return "NO";
+
+    }
+}
