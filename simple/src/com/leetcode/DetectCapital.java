0b81b994e2aef6d5e27163672c2f0d532fb2351b
kirkwang <kirkwangke@gmail.com>
2019-05-01 13:13:28 -0700

    DetectCapital

---
+++ simple/src/com/leetcode/DetectCapital.java
@@ -0,0 +1,33 @@
+/*
+ * 5/1/19 12:59 PM : for the current date and time;
+ * 2019 : to access the year information of the current date;
+ * kirkwang : the currently used username;
+ */
+
+package com.leetcode;
+
+/*
+https://leetcode.com/problems/detect-capital/submissions/
+ */
+public class DetectCapital {
+    public static void main(String[] args) {
+
+        String s = "Google";
+        String s1 = "FlaG";
+
+
+        boolean result = detectCapitalUse(s);
+        System.out.println(result);
+
+    }
+
+    public static boolean detectCapitalUse(String word) {
+        return word.equals(word.toUpperCase()) || isLowerCase(word) || isLowerCase(word.substring(1)) && Character.isUpperCase(word.charAt(0));
+    }
+
+    public static boolean isLowerCase(String s) {
+
+        return s.equals(s.toLowerCase());
+
+    }
+}
