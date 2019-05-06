1737e3544a7b51eb120abbba895f09e3cb7fa8e3
kirkwang <kirkwangke@gmail.com>
2019-05-06 15:14:25 -0700

    IceCreamParlor

---
+++ simple/src/com/kirkwang/HackerRank/Pairs.java
@@ -0,0 +1,10 @@
+/*
+ * 5/6/19 3:13 PM : for the current date and time;
+ * 2019 : to access the year information of the current date;
+ * kirkwang : the currently used username;
+ */
+
+package com.kirkwang.HackerRank;
+
+public class Pairs {
+}
