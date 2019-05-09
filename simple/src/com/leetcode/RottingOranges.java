798bb2bf4e19646ed3ecf02d631028d9e540105c
kirkwang <kirkwangke@gmail.com>
2019-05-08 20:15:12 -0700

    RottingOranges

--- simple/src/com/leetcode/RottingOranges.java
+++ simple/src/com/leetcode/RottingOranges.java
@@ -6,7 +6,26 @@
 
 package com.leetcode;
 
+import java.util.Arrays;
+
+/*
+https://leetcode.com/problems/rotting-oranges/
+ */
 public class RottingOranges {
+
+    public static int orangesRotting(int[][] grid) {
+        countOnes(grid);
+        System.out.println(grid[0].length);
+        return 0;
+    }
+
+    public static int countOnes(int[][] grid) {
+
+        return Arrays.stream(grid).mapToInt(line -> (int) Arrays.stream(line).filter(x -> x == 1).count()).sum();
+    }
+
     public static void main(String[] args) {
+        int[][] input = {{2, 1, 1}, {1, 1, 0}, {0, 1, 1}};
+        RottingOranges.orangesRotting(input);
     }
 }
