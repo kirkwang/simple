ec237616cc5ee127dea62c6b356e5245ea17edac
kirkwang <kirkwangke@gmail.com>
2021-03-09 22:40:04 -0800

    update String2Minutes String2Min

--- simple/src/com/kirkwang/Codility/String2Minutes.java
+++ simple/src/com/kirkwang/Codility/String2Minutes.java
@@ -8,24 +8,29 @@ import java.util.concurrent.TimeUnit;
 public class String2Minutes {
 
 
-  public static int String2Minutes(String time) {
-    String[] split = time.split(":");
-    long minutes = 0;
-    if (split.length == 2) {
-      minutes = TimeUnit.HOURS.toMinutes(Integer.parseInt(split[0])) +
-          Integer.parseInt(split[1]);
-      System.out.println(minutes);
+    public static int String2Minutes(String time) {
+        String[] split = time.split(":");
+        long minutes = 0;
+        if (split.length == 2) {
+            minutes = TimeUnit.HOURS.toMinutes(Integer.parseInt(split[0])) +
+                    Integer.parseInt(split[1]);
+            System.out.println(minutes);
+        }
+        return (int) minutes;
     }
-    return (int) minutes;
-  }
+
+    public static int String2Min(String time) {
+        String[] sp = time.split(":");
+        if (sp.length > 2) return -1;
+        int hours = Integer.parseInt(sp[0]) * 60;
+
+        return hours + Integer.parseInt(sp[1]);
+    }
+
+    public static void main(String[] args) {
 
 
-  public static void main(String[] args) {
-
-    int[] n = new int[]{1, 0, 0, 1, 0, 0}; //5, 4, 3, 2, 1
-    //  '10:00', '13:21'
-
-    System.out.println(String2Minutes("1:21"));
-  }
+        System.out.println(String2Min("00:21"));
+    }
 
 }
