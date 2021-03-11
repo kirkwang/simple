e50a9cbe445f932a87f52f306d082bbc9423b7b0
kirkwang <kirkwangke@gmail.com>
2021-03-11 10:12:19 -0800

    update majorityElement

--- simple/src/com/kirkwang/easy/MajorityElement.java
+++ simple/src/com/kirkwang/easy/MajorityElement.java
@@ -17,19 +17,19 @@ You may assume that the array is non-empty and the majority element always exist
 public class MajorityElement {
 
     protected static Integer majorityElement(int[] numbs) {
+        Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
 
-        Map<Integer, Integer> map = new HashMap();
         for (int i = 0; i < numbs.length; i++) {
-            if (map.containsKey(numbs[i])) {
-                Integer count = map.get(numbs[i]);
-                map.put(numbs[i], count + 1);
+            if (hm.containsKey(numbs[i])) {
+                Integer temp = hm.get(numbs[i]);
+                hm.put(numbs[i], temp + 1);
+
             } else {
-                map.put(numbs[i], 1);
+                hm.put(numbs[i], 1);
             }
         }
-        for (int i : map.keySet()) {
-            int count = map.get(i);
-            System.out.println("count =>" + count);
+        for (Integer i : hm.keySet()) {
+            int count = hm.get(i);
             if (count > numbs.length / 2) {
                 return i;
             }
