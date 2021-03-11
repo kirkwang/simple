062c35bce74702a7d08d6e78d2460d0cbff97963
kirkwang <kirkwangke@gmail.com>
2021-03-11 13:50:39 -0800

    update spike logic

--- simple/src/com/kirkwang/easy/Spike.java
+++ simple/src/com/kirkwang/easy/Spike.java
@@ -10,17 +10,23 @@ class Spike {
     public
     static int findSpike(int[] input) {
 
+        if (input[0] > input[1]) {
+            return input[0];
+        }
+        if (input[input.length - 1] > input[input.length - 2]) {
+            return input[input.length - 1];
+        }
+
         for (int i = 1; i < input.length; i++) {
             if (input[i - 1] > input[i]) {
                 return input[i - 1];
-
             }
         }
         return input[input.length - 1];
     }
 
     public static void main(String[] args) {
-        int[] input = new int[]{1, 2, 3, 8, 6, 5};
+        int[] input = new int[]{1, 2, 3, 8, 6, 10};
         System.out.println(findSpike(input));
     }
 }
