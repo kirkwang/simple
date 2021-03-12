8ec9a4bdcd54398afc42d8de851ebdd705b9d0f8
kirkwang <kirkwangke@gmail.com>
2021-03-11 16:14:56 -0800

    few ways to print ArrayListSort

--- simple/src/com/kirkwang/easy/ArrayListSort.java
+++ simple/src/com/kirkwang/easy/ArrayListSort.java
@@ -1,8 +1,6 @@
 package com.kirkwang.easy;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class ArrayListSort {
 
@@ -13,11 +11,25 @@ public class ArrayListSort {
         int a = 1234512345;
 
         for (int i = 0; i < al.size(); i++) {
-            System.out.println(al.get(i));
+            System.out.println(" Al int i=0 -> " + al.get(i));
         }
-
+        Map<Integer, Character> myMap = new HashMap();
+        myMap.put(1, 'a');
+        myMap.put(2, 'b');
+        myMap.put(3, 'c');
+        myMap.put(4, 'd');
+        for (int i = 0; i < myMap.size(); i++) {
+            System.out.println(" myMap.get(i) int i=0 -> " + al.get(i));
+        }
+        for (Integer i : myMap.keySet()) { //This is for key only
+            System.out.println(" Map.Entry i  -> " + i);
+        }
+        for (Map.Entry e : myMap.entrySet()) { //This will print map pair 1 = a ....
+            System.out.println(" Map.Entry i key hash  -> " + e.getKey().hashCode());
+            System.out.println(" Map.Entry i value hash -> " + e.getValue().hashCode());
+        }
         for (Object object : al) {
-            System.out.println(object);
+            System.out.println(" object " + object);
         }
 
         al.forEach(System.out::println);
