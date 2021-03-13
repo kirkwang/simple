c31a7cc3c439cee16dde9160317030c1f9f43f7c
kirkwang <kirkwangke@gmail.com>
2021-03-12 20:56:39 -0800

    update TreeNode

--- simple/src/com/kirkwang/basicStructure/TreeNode.java
+++ simple/src/com/kirkwang/basicStructure/TreeNode.java
@@ -1,21 +1,26 @@
 /*
- * 3/24/19 11:00 PM : for the current date and time;
- * 2019 : to access the year information of the current date;
- * kirkwang : the currently used username;
+  3/24/19 11:00 PM : for the current date and time;
+  2019 : to access the year information of the current date;
+  kirkwang : the currently used username;
  */
 
 package com.kirkwang.basicStructure;
 
 public class TreeNode {
-    public TreeNode left;
-    public TreeNode right;
     int val;
+    TreeNode left;
+    TreeNode right;
 
-    TreeNode(int x) {
-        val = x;
+    TreeNode() {
     }
 
-    public int getVal() {
-        return val;
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
