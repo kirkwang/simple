0f047ceb2a35d4d698b6f3f6a2b9e5f5ed16466d
kirkwang <kirkwangke@gmail.com>
2021-03-18 15:57:57 -0700

    update string type from ArrayList to []

--- simple/src/com/kirkwang/easy/Unique_Number_of_Occurrences.java
+++ simple/src/com/kirkwang/easy/Unique_Number_of_Occurrences.java
@@ -16,18 +16,24 @@ package com.kirkwang.easy;
         Output: true
 */
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class Unique_Number_of_Occurrences {
-    public static boolean uniqueOccurrences(int[] arr) {
+    public static Object[] uniqueOccurrences(int[] arr) {
 
-        HashMap<Integer, Integer> myMap = new HashMap<>();
-        // for (int i : arr){
-        for (int i = 0; i < arr.length; i++) {
-            myMap.put(arr[i], myMap.getOrDefault(arr[i], 0) + 1);
+        HashMap<Integer, Integer> myMap = new HashMap<Integer, Integer>();
+        for (int i : arr) {
+            //  for (int i = 0; i < arr.length; i++) {
+            myMap.put(i, myMap.getOrDefault(i, 1) + 1);
         }
+        ArrayList<Integer> abc = new ArrayList<>();
+        abc.toArray();
 
+        HashSet<Integer> set = new HashSet<>(myMap.values());
+        return abc.toArray();
+/*
         HashSet mySet = new HashSet();
         for (Integer num : myMap.values()) {
             if (!mySet.add(num)) {
@@ -35,11 +41,13 @@ public class Unique_Number_of_Occurrences {
             }
         }
         return true;
+*/
     }
 
     public static void main(String[] args) {
 
-        int[] target = new int[]{1, 2};
+        //    int[] target = new int[]{1, 2};
+        int[] target = new int[]{1, 2, 2, 1, 1, 3};
         System.out.print(uniqueOccurrences(target));
 
     }
