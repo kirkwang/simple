311a0f0c48c2cd7ecfe8742c209c59ac70d2a430
kirkwang <kirkwangke@gmail.com>
2021-03-18 16:26:30 -0700

    refactor MaxProductOfThree

--- simple/src/com/kirkwang/Codility/MaxProductOfThree.java
+++ simple/src/com/kirkwang/Codility/MaxProductOfThree.java
@@ -1,3 +1,5 @@
+package com.kirkwang.Codility;
+
 /**
  * Created by kewang on 11/15/17.
  *
@@ -20,6 +22,7 @@
  * For example, given array A such that: A[0] = -3 A[1] = 1 A[2] = 2 A[3] = -2 A[4] = 5 A[5] = 6
  *
  * the function should return 60, as the product of triplet (2, 4, 5) is maximal.
+ * https://github.com/Mickey0521/Codility/blob/master/MaxProductOfThree.java
  */
 public class MaxProductOfThree {
 
@@ -30,7 +33,7 @@ public class MaxProductOfThree {
 
     java.util.Arrays.sort(A);
     int total = A[A.length - 3] * A[A.length - 2] * A[A.length - 1];
-    int total2 = A[A.length - 1] * A[0] * A[1];  //why , I don't get this part
+    int total2 = A[A.length - 1] * A[0] * A[1];  //This case is for -neg * -neg *  max -Pos
     return total > total2 ? total : total2;
 
   }
