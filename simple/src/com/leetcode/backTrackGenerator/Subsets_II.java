4a6a9aabec25a677985347ca1b0b86edd0217901
kirkwang <kirkwangke@gmail.com>
2021-04-09 10:53:48 -0700

    update Subsets_II

--- simple/src/com/leetcode/backTrackGenerator/Subsets_II.java
+++ simple/src/com/leetcode/backTrackGenerator/Subsets_II.java
@@ -6,52 +6,45 @@ import java.util.List;
 
 /**
  * Created by kewang on 12/15/17.
- *
- *
- * https://leetcode.com/problems/subsets-ii/description/
- * https://leetcode.com/problems/subsets/discuss/
- *
- *
+ * <p>
+ * <p>
+ * https://leetcode.com/problems/subsets-ii/description/ https://leetcode.com/problems/subsets/discuss/
+ * <p>
+ * <p>
  * Given a collection of integers that might contain duplicates, nums, return all possible subsets
  * (the power set).
- *
+ * <p>
  * Note: The solution set must not contain duplicate subsets.
- *
+ * <p>
  * For example, If nums = [1,2,2], a solution is:
- *
+ * <p>
  * [ [2], [1], [1,2,2], [2,2], [1,2], [] ]
  */
 public class Subsets_II {
 
   public static void main(String[] args) {
-      List result = subsets(new int[]{1, 2, 2, 3, 3});
+    List result = subsets(new int[]{1, 2, 2});
     System.out.println("This is the result " + result);
   }
 
   public static List<List<Integer>> subsets(int[] nums) {
-    List<List<Integer>> list = new ArrayList<>();
-    if (nums == null) {
-      return list;
-    }
-
+    List<List<Integer>> result = new ArrayList<>();
     Arrays.sort(nums);
-    generateSubSet(list, new ArrayList<Integer>(), nums, 0);
 
-    return list;
+    permute(result, new ArrayList(), nums, 0);
+    return result;
   }
 
-  private static void generateSubSet(List<List<Integer>> list, List objects,
-      int[] nums, int index) {
-    list.add(new ArrayList<>(objects));
-
+  public static void permute(List list, List subList, int[] nums, int index) {
+    list.add(new ArrayList<>(subList));
     for (int i = index; i < nums.length; i++) {
-      //if loop starts and current item is same as previous , skip
-      if (i > index && nums[i] == nums[i - 1]) {
+      if (i > index && nums[i - 1] == nums[i]) {
         continue;
       }
-      objects.add(nums[i]);
-      generateSubSet(list, objects, nums, i + 1);
-      objects.remove(objects.size() - 1);
+      subList.add(nums[i]);
+      permute(list, subList, nums, i + 1);
+      subList.remove((subList.size() - 1));
     }
   }
+
 }
