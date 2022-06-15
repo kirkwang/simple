79c6af4c811e38694d0df4b7460ab8e117479131
kirkwang <kirkwangke@gmail.com>
2022-06-15 15:17:14 -0700

    refactor detect capital, primenumber

--- simple/src/com/leetcode/PrimeNumber.java
+++ simple/src/com/leetcode/PrimeNumber.java
@@ -11,10 +11,12 @@ public class PrimeNumber {
     public static boolean isPrime(int n) {
         if (n < 2) {
             return false;
-        } else if (n == 2) {     // account for even numbers now, so that we can do i+=2 in loop below
-            return true;
-        } else if (n % 2 == 0) { // account for even numbers now, so that we can do i+=2 in loop below
+        } else if (n % 2
+            == 0) { // account for even numbers now, so that we can do i+=2 in loop below
             return false;
+        } else if (n
+            == 2) {     // account for even numbers now, so that we can do i+=2 in loop below
+            return true;
         }
         int sqrt = (int) Math.sqrt(n);
         for (int i = 3; i <= sqrt; i += 2) { // skips even numbers for faster results
