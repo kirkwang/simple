75eaa6c851d1d9afb12d6e2059f969987ace58c8
kirkwang <kirkwangke@gmail.com>
2022-06-15 14:20:48 -0700

    refactor binary search

--- simple/src/com/leetcode/BinarySearch.java
+++ simple/src/com/leetcode/BinarySearch.java
@@ -7,25 +7,26 @@
 package com.leetcode;
 
 public class BinarySearch {
-    public static void main(String[] args) {
-        int[] sortedArray = new int[]{1, 3, 5, 6};
-        int target = 5;
-        System.out.print(String.valueOf(searchInsert(sortedArray, target)));
+
+  public static void main(String[] args) {
+    int[] sortedArray = new int[]{1, 3, 5, 6};
+    int target = 5;
+    System.out.print(" index location " + searchInsert(sortedArray, target));
+  }
+
+  public static int searchInsert(int[] nums, int target) {
+
+    int low = 0;
+    int high = nums.length - 1;
+    while (low < high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] < target) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
     }
 
-    public static int searchInsert(int[] nums, int target) {
-
-        int low = 0;
-        int high = nums.length - 1;
-
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return low;
-    }
+    return low;
+  }
 }
