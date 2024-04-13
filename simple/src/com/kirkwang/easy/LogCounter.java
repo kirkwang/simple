84ba44f6f3fc721e138cecea402f5d06fb3135c4
kirkwang <kirkwangke@gmail.com>
2024-04-13 10:55:14 -0700

    chore: update LogCounter

--- simple/src/com/kirkwang/easy/LogCounter.java
+++ simple/src/com/kirkwang/easy/LogCounter.java
@@ -10,7 +10,7 @@ public class LogCounter {
 
   public static void main(String[] args) throws IOException {
     LogCounter logCounter = new LogCounter();
-    List<String> allLines = logCounter.fileReader("resources/file.log");
+    List<String> allLines = logCounter.fileReader("simple/resources/file.log");
     HashMap<String, String[]> mMap = logCounter.buildMap(allLines);
     for (String[] s : mMap.values()) {
       System.out.println(
@@ -37,20 +37,20 @@ public class LogCounter {
     HashMap<String, String[]> myMap = new HashMap();
     //111.222.333.123 , HOME , [01/Feb/1998:01:08:39 -0800] , 2 , 4;
     for (String i : input) {
-      String[] spitted = i.split(",");
+      String[] chopped = i.split(",");
 
-      if (myMap.containsKey(spitted[0])) {
-        String[] temp = myMap.get(spitted[0]);
+      if (myMap.containsKey(chopped[0])) {
+        String[] temp = myMap.get(chopped[0]);
         temp[1] = String.valueOf(Integer.valueOf(temp[1]) + 1);
 
         temp[3] = String.valueOf(
-            Integer.valueOf(temp[3].trim()) + Integer.valueOf(spitted[3].trim()));
+            Integer.valueOf(temp[3].trim()) + Integer.valueOf(chopped[3].trim()));
         temp[4] = String.valueOf(
-            Integer.valueOf(temp[4].trim()) + Integer.valueOf(spitted[4].trim()));
-        myMap.put(spitted[0], temp);
+            Integer.valueOf(temp[4].trim()) + Integer.valueOf(chopped[4].trim()));
+        myMap.put(chopped[0], temp);
       } else {
-        spitted[1] = "1";
-        myMap.put(spitted[0], spitted);
+        chopped[1] = "1";
+        myMap.put(chopped[0], chopped);
       }
     }
     return myMap;
