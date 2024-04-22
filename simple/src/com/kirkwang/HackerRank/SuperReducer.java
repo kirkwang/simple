d9fadb1179f4179284cb166bd59dce671c59a08b
kirkwang <kirkwangke@gmail.com>
2024-04-21 23:22:12 -0700

    chore: update SuperReducer

--- simple/src/com/kirkwang/HackerRank/SuperReducer.java
+++ simple/src/com/kirkwang/HackerRank/SuperReducer.java
@@ -1,8 +1,8 @@
 package com.kirkwang.HackerRank;
 
-import java.util.Stack;
 
 /**
+ * https://www.hackerrank.com/challenges/reduced-string/problem
  * Created by kewang on 10/31/17. Problem Statement Steve has a string, S, consisting of n lowercase
  * English alphabetic >letters. In one operation, he can delete any pair of adjacent letters with
  * same >value. For example, string "aabcc" would become either "aab" or "bcc" >after operation.
@@ -22,11 +22,18 @@ import java.util.Stack;
  *
  * <p>Sample Output abd
  */
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class SuperReducer {
 
     // Complete the superReducedString function below.
     static String superReducedString(String s) {
-        Stack<Character> stack = new Stack();
+        Stack<Character> stack = new Stack<>();
 
         for (char c : s.toCharArray()) {
             if (stack.empty()) {
@@ -43,19 +50,47 @@ public class SuperReducer {
         for (Character c : stack) {
             sb.append(c);
         }
-        return sb.toString();
-        //return stack.isEmpty() ? "Empty String" : (String) stack.stream().map(i -> i.toString()).collect(Collectors.joining());
+
+        return stack.isEmpty() ? "Empty String" : stack.stream().map(i -> i.toString()).collect(Collectors.joining());
     }
 
+    public static String superReducedString_v2(String input) {
+       // This method doesn't work because that it reduce the whole string.
+        // it should only reduce the nearby string.
+        //This is wrong"zztqooauhujtmxnsbzpy" -> "abhjmnpqsxyz"
+        //It should be "zztqooauhujtmxnsbzpy" -> "tqauhujtmxnsbzpy"
+        if (input.isEmpty()){
+            return "Empty String";
+        }
+        HashMap<Character, Integer> mMap = new HashMap();
+        for (Character i : input.toCharArray()) {
+            mMap.put(i, mMap.getOrDefault(i, 0) + 1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Character, Integer> entry : mMap.entrySet()) {
+
+            if (entry.getValue() % 2 != 0) {
+                sb.append(entry.getKey());
+            }
+        }
+
+        return sb.length() == 0 ? "Empty String" : sb.toString();
+    }
 
     public static void main(String[] args) {
-        String input = "aaaabccddd";
+        String input = "zztqooauhujtmxnsbzpykwlvpfyqijvdhuhiroodmuxiobyvwwxupqwydkpeebxmfvxhgicuzdealkgxlfmjiucasokrdznmtlwh";
+        String reduced;
+        // input ="aa";
+        Scanner scanner = new Scanner(System.in);
+        input = scanner.next();
+        reduced = superReducedString(input.trim().replace("[^a-zA-Z]", ""));
 
-        String reduced = superReducedString(input);
 
-        System.out.println(input + " simple input string");
-        System.out.println(reduced + " reduced string");
+        // reduced = superReducedString_v2(input.trim().replace("[^a-zA-Z]", ""));
+        System.out.println(reduced);
 
+/*
         int index = 0;
         int len = input.length() - 1;
 
@@ -72,6 +107,6 @@ public class SuperReducer {
                 continue;
             }
             index++;
-        }
+        } */
     }
 }
