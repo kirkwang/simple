d0ff27ef7549629f7a8a6277d79559438ea9041b
kirkwang <kirkwangke@gmail.com>
2024-04-24 15:41:07 -0700

    chore: update MaxProfit

--- simple/src/com/kirkwang/easy/MaxProfit.java
+++ simple/src/com/kirkwang/easy/MaxProfit.java
@@ -1,11 +1,11 @@
-package com.kirkwang.HackerRank;
+package com.kirkwang.easy;
 
 
 /**
  * Created by kewang on 11/15/17. For example, consider the following array A consisting of six
  * elements such that: A[0] = 23171 A[1] = 21011 A[2] = 21123 A[3] = 21366 A[4] = 21013 A[5] =
  * 21367
- * <p>
+ * <a href="https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/">...</a>
  * If a share was bought on day 0 and sold on day 2, a loss of 2048 would occur because A[2] − A[0]
  * = 21123 − 23171 = −2048. If a share was bought on day 4 and sold on day 5, a profit of 354 would
  * occur because A[5] − A[4] = 21367 − 21013 = 354. Maximum possible profit was 356. It would occur
@@ -36,7 +36,7 @@ public class MaxProfit {
     public static void main(String[] args) {
 
         int[] A = new int[]{23171, 21011, 21123, 21366, 21013, 21367};
-        int[] n = new int[]{5, 4, 3, 2, 1}; //5, 4, 3, 2, 1
+        int[] n = new int[]{7,1,5,3,6,4}; //5, 4, 3, 2, 1
 
 
         System.out.println(MaxProfit(n));
