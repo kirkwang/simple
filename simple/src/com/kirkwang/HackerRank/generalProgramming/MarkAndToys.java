ef3c4f8627cee801a25ff5c4c291fc1c6c71e03b
kirkwang <kirkwangke@gmail.com>
2024-04-23 22:08:16 -0700

    chore: update MarkAndToys

--- simple/src/com/kirkwang/HackerRank/generalProgramming/MarkAndToys.java
+++ simple/src/com/kirkwang/HackerRank/generalProgramming/MarkAndToys.java
@@ -6,25 +6,38 @@
 
 package com.kirkwang.HackerRank.generalProgramming;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
+/*
+https://www.hackerrank.com/challenges/mark-and-toys/problem
+ */
 public class MarkAndToys {
     public static void main(String[] args) {
 
         int[] input = {50, 1, 12, 5, 111, 200, 1000, 10};
         int total = 50;
-        System.out.println(maximumToys(input, total));
+        List<Integer> prices = new ArrayList<>();
+        for (int i : input) {
+            prices.add(i);
+        }
+
+        System.out.println(maximumToys(prices, total));
     }
 
-    static int maximumToys(int[] prices, int k) {
-        Arrays.sort(prices);
+    static int maximumToys(List<Integer> prices, int k) {
+        if (k <= 0 || prices.isEmpty()) {
+            return 0;
+        }
+        prices.sort(Comparator.naturalOrder());
         int sum = 0;
         int i;
-        for (i = 0; i < prices.length; i++) {
-            if (sum >= k) {
-                return i - 1;
+        for (i = 0; i < prices.size(); i++) {
+            if (sum < k) {
+                sum = sum + prices.get(i);
             } else {
-                sum = sum + prices[i];
+                return i - 1;
             }
         }
         return i - 1;
