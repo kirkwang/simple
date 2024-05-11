ad1cfe59568bc3224ab6935c5446b3bc425bffcc
kirkwang <kirkwangke@gmail.com>
2024-05-11 09:54:31 -0700

    chore: update , RemoveArrayElement

--- simple/src/com/kirkwang/easy/RemoveArrayElement.java
+++ simple/src/com/kirkwang/easy/RemoveArrayElement.java
@@ -12,9 +12,23 @@ The order of elements can be changed. It doesn't matter what you leave beyond th
 public class RemoveArrayElement {
 
     public static void main(String[] args) {
+        //  int[] inputArray = new int[]{1, 2, 2, 2, 3, 4, 4};
+        int[] inputArray = new int[]{0, 1, 2, 2, 3, 0, 4, 2};
+
         RemoveArrayElement rd = new RemoveArrayElement();
-        int[] removed = rd.removeArrayElement(new int[]{1, 4, 2, 2, 3, 4, 4}, 4);
-        for (int aRemoved : removed) {
+        int result = rd.removeElement(inputArray, 4);
+        System.out.println(result + " removeElement result ");
+        rd.printArray(inputArray);
+
+        result = rd.removeElement_v1(inputArray, 4);
+
+        System.out.println(result + " removeElement_v1 result ");
+        int[] removed = rd.removeArrayElement(inputArray, 4);
+        rd.printArray(removed);
+    }
+
+    private void printArray(int[] input) {
+        for (int aRemoved : input) {
             System.out.print(aRemoved + " ");
         }
     }
@@ -31,5 +45,35 @@ public class RemoveArrayElement {
             numbs[index++] = 0;
         }
         return numbs;
+    }
+
+    public int removeElement(int[] nums, int val) {
+        if (nums.length == 0) return 0;
+        int uniqe = 0;
+        int index = 0;
+
+        while (index < nums.length) {
+            if (nums[index] != val) {
+                nums[uniqe] = nums[index];
+                uniqe++;
+            }
+            index++;
+        }
+        return uniqe;
+
+    }
+
+    public int removeElement_v1(int[] nums, int val) {
+        if (nums == null) {
+            return 0;
+        }
+        int i = 0;
+        for (int n : nums) {
+            if (n != val) {
+                nums[i] = n;
+                i++;
+            }
+        }
+        return i;
     }
 }
