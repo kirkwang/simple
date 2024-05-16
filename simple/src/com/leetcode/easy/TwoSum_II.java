297fe614925230c9c99c0b8d249f48918b41f505
kirkwang <kirkwangke@gmail.com>
2024-05-16 14:02:21 -0700

    update TwoSum_II

--- simple/src/com/leetcode/easy/TwoSum_II.java
+++ simple/src/com/leetcode/easy/TwoSum_II.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 /**
  * Created by kewang on 12/22/17.
  * <p>
- * https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+ * <a href="https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/">...</a>
  * <p>
  * Given an array of integers that is already sorted in ascending order, find two numbers such that
  * they add up to a specific target number.
@@ -22,32 +22,36 @@ import java.util.HashMap;
 
 public class TwoSum_II {
 
-    public static int[] TwoSum_II(int[] numbs, int index, int total) {
 
-        int head = index;
-        int tail = numbs.length - 1;
-
-        while (head <= tail && numbs[head] + numbs[tail] != total) {
-            int sum = numbs[tail] + numbs[head];
-            if (sum < total) {
+    public static int[] twoSum(int[] numbers, int target) {
+        int head = 0;
+        int tail = numbers.length - 1;
+        while (head <= tail) {
+            int sum = numbers[head] + numbers[tail];
+            if (sum == target) {
+                return new int[]{++head, ++tail};
+            } else if (sum < target) {
                 head++;
             } else {
                 tail--;
             }
         }
-        return new int[]{head + 1, tail + 1};
+        return new int[]{};
     }
 
-    public static int[] TwoSum_III(int[] numbs, int total) {
-        if (numbs == null || numbs.length < 2) {
-            return null;
+    public static int[] TwoSum_III(int[] numbers, int target) {
+        //slow but less memory
+        //Beats 16.91% of users with Java
+        //Beats 97.08% of users with Java
+        if (numbers == null || numbers.length < 2) {
+            return new int[0];
         }
-        HashMap<Integer, Integer> hashMap = new HashMap();
-        for (int i = 0; i < numbs.length; i++) {
-            if (hashMap.containsKey(numbs[i])) {
-                return new int[]{hashMap.get(numbs[i]), i};
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (hashMap.containsKey(numbers[i])) {
+                return new int[]{hashMap.get(numbers[i]) + 1, ++i};
             } else {
-                hashMap.put(total - numbs[i], i);
+                hashMap.put(target - numbers[i], i);
             }
         }
 
@@ -55,7 +59,7 @@ public class TwoSum_II {
     }
 
     public static void main(String[] args) {
-        int[] result = TwoSum_II(new int[]{1, 2, 7, 15}, 0, 9);
+        int[] result = twoSum(new int[]{2, 7, 11, 15}, 9);
         result = TwoSum_III(new int[]{1, 2, 7, 15}, 9);
         for (int sum : result) {
             System.out.println(sum);
