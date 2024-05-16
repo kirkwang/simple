481f5ce99897a09690027aa5bd616a2253369eb2
kirkwang <kirkwangke@gmail.com>
2024-05-15 22:42:22 -0700

    update PalindromeString

--- simple/src/com/leetcode/easy/PalindromeString.java
+++ simple/src/com/leetcode/easy/PalindromeString.java
@@ -3,26 +3,34 @@ package com.leetcode.easy;
 /**
  * Created by kewang on 12/27/15.
  */
-/*
+
+/**
+ *
+ * https://leetcode.com/problems/valid-palindrome/
   Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
 
 For example,
 "A man, a plan, a canal: Panama" is a palindrome.
 "race a car" is not a palindrome.
-   */
+ **/
 public class PalindromeString {
 
     public static void main(String[] args) {
         String trimedInput = "amanaplanacanalpanama";
 
         String source = "A man, a plan, a canal: Panama";
-        source = source.replaceAll("[^a-zA-Z0-9]]", "").toLowerCase();
+
         System.out.println(PalindromeString.palindromeString(source));
     }
 
-    public static boolean palindromeString(String input) {
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+    public static boolean palindromeString(String s) {
+        if (s == null) {
+            return false;
+        }
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
                 return false;
             }
         }
