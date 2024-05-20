094db3c39e75573338db746aedb5a723805cd1c3
kirkwang <kirkwangke@gmail.com>
2024-05-20 10:56:31 -0700

    update MyQueue

---
+++ simple/src/com/leetcode/easy/MyQueue.java
@@ -0,0 +1,79 @@
+package com.leetcode.easy;
+/*
+  https://leetcode.com/problems/implement-queue-using-stacks/submissions/1263312563/
+  Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+  Input
+  ["MyQueue", "push", "push", "peek", "pop", "empty"]
+  [[], [1], [2], [], [], []]
+  Output
+  [null, null, null, 1, 1, false]
+  <p>
+  Explanation
+  MyQueue myQueue = new MyQueue();
+  myQueue.push(1); // queue is: [1]
+  myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+  myQueue.peek(); // return 1
+  myQueue.pop(); // return 1, queue is [2]
+  myQueue.empty(); // return false
+ */
+
+import org.testng.Assert;
+
+import java.util.Stack;
+
+public class MyQueue {
+    Stack<Integer> one;
+    Stack<Integer> two;
+
+    public MyQueue() {
+        one = new Stack<>();
+        two = new Stack<>();
+    }
+
+    public static void main(String[] args) {
+        MyQueue obj = new MyQueue();
+        obj.push(1);
+
+        Assert.assertEquals(obj.pop(), 1);
+        Assert.assertTrue(obj.empty());
+        int param_3 = obj.peek();
+        Assert.assertEquals(param_3, 0);
+
+    }
+
+    public void push(int x) {
+        one.push(x);
+    }
+
+    public int pop() {
+        if (one.empty()) {
+            return 0;
+        }
+        while (!one.empty()) {
+            two.push(one.pop());
+        }
+        int result = two.pop();
+        while (!two.empty()) {
+            one.push(two.pop());
+        }
+        return result;
+    }
+
+    public int peek() {
+        if (one.empty()) {
+            return 0;
+        }
+        while (!one.empty()) {
+            two.push(one.pop());
+        }
+        int result = two.peek();
+        while (!two.empty()) {
+            one.push(two.pop());
+        }
+        return result;
+    }
+
+    public boolean empty() {
+        return one.empty();
+    }
+}
