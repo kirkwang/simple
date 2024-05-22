19128b0eddfee72eed067502121496b3b868c26b
kirkwang <kirkwangke@gmail.com>
2024-05-22 14:19:24 -0700

    update TwoSumProblem

--- simple/src/com/leetcode/easy/TwoSumProblem.java
+++ simple/src/com/leetcode/easy/TwoSumProblem.java
@@ -25,9 +25,7 @@ public class TwoSumProblem {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
+                    return new int[]{i, j};
                 }
             }
         }
@@ -37,6 +35,9 @@ public class TwoSumProblem {
     public static void main(String[] args) {
         int[] result = function(new int[]{3, 3}, 6);
         Assert.assertEquals(result, new int[]{0, 1});
+
+        Assert.assertEquals(function(new int[]{2, 7, 11, 15}, 9), new int[]{0, 1});
+        Assert.assertEquals(function(new int[]{3, 2, 4}, 6), new int[]{1, 2});
 
     }
 }
