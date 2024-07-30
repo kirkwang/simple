49f6bfbb614b4f0cf75ec4b652cb62b630eb0241
kirkwang <kirkwangke@gmail.com>
2024-07-30 15:40:04 -0700

    update SubdomainVisitCount

--- simple/src/com/leetcode/medium/SubdomainVisitCount.java
+++ simple/src/com/leetcode/medium/SubdomainVisitCount.java
@@ -4,7 +4,22 @@
  * kirkwang : the currently used username;
  */
 
-package com.leetcode.easy;
+package com.leetcode.medium;
+/*
+Example 1:
+
+Input: cpdomains = ["9001 discuss.leetcode.com"]
+Output: ["9001 leetcode.com","9001 discuss.leetcode.com","9001 com"]
+Explanation: We only have one website domain: "discuss.leetcode.com".
+As discussed above, the subdomain "leetcode.com" and "com" will also be visited. So they will all be visited 9001 times.
+Example 2:
+
+Input: cpdomains = ["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
+Output: ["901 mail.com","50 yahoo.com","900 google.mail.com","5 wiki.org","5 org","1 intel.mail.com","951 com"]
+Explanation: We will visit "google.mail.com" 900 times, "yahoo.com" 50 times, "intel.mail.com" once and "wiki.org" 5 times.
+For the subdomains, we will visit "mail.com" 900 + 1 = 901 times, "com" 900 + 50 + 1 = 951 times, and "org" 5 times.
+
+ */
 
 import java.util.HashMap;
 import java.util.List;
@@ -20,21 +35,18 @@ public class SubdomainVisitCount {
 
     public static List<String> subdomainVisits(String[] cpdomains) {
         HashMap<String, Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < cpdomains.length; i++) {
-            String[] domainSplit = cpdomains[i].split(" ");
-            int count = Integer.valueOf(domainSplit[0]);
+        for (String cpdomain : cpdomains) {
+            String[] domainSplit = cpdomain.split(" ");
+            int count = Integer.parseInt(domainSplit[0]);
             String domain = domainSplit[1];
 
             int index = 0;
-            while (true) {
+            do {
                 String subdomain = domain.substring(index); //something.domain.com has index 0.1.2 as on line 32
                 hashMap.put(subdomain, hashMap.getOrDefault(subdomain, 0) + count);
                 index = domain.indexOf(".", index); //index has begin index or start index
-                if (index++ < 0) {
-                    break;
-                }
-                // index++;
-            }
+
+            } while (index++ >= 0);
         }
         return hashMap.entrySet().stream().map(stringIntegerEntry -> (stringIntegerEntry.getValue() + " " + stringIntegerEntry.getKey())).collect(Collectors.toList());
     }
