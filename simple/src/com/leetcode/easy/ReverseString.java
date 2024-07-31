142b265fded749ce9e21e11946ef4785c9bb016a
kirkwang <kirkwangke@gmail.com>
2024-07-31 15:16:29 -0700

    update ReverseString

--- simple/src/com/leetcode/easy/ReverseString.java
+++ simple/src/com/leetcode/easy/ReverseString.java
@@ -2,64 +2,38 @@ package com.leetcode.easy;
 
 import org.testng.Assert;
 
-/**
- * Created by amp on 9/24/2015.
- * <a href="https://leetcode.com/problems/reverse-words-in-a-string/">...</a>
- * Example 1:
- * Input: s = "the sky is blue"
- * Output: "blue is sky the"
- * <p>
- * Example 2:
- * Input: s = "  hello world  "
- * Output: "world hello"
- * Explanation: Your reversed string should not contain leading or trailing spaces.
- * <p>
- * Example 3:
- * Input: s = "a good   example"
- * Output: "example good a"
- * Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+/*
+https://leetcode.com/problems/reverse-string/description/
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
  */
 public class ReverseString {
-    public static char[] reverseString(char[] s) {
-       /*
-       Input: s = ["h","e","l","l","o"]
-       Output: ["o","l","l","e","h"]
-        */
+    public static void reverseString(char[] s) {
         if (s == null) {
-            return s;
+            return;
         }
         for (int i = 0; i < s.length / 2; i++) {
             char temp = s[i];
             s[i] = s[s.length - 1 - i];
             s[s.length - 1 - i] = temp;
         }
-        return s;
     }
-    public static String reverseWords(String s) {
-        if (s == null || s.isEmpty()) {
-            return s;
-        }
-        String noSpaces = s.trim().replaceAll("\\s+", ":");
-        String[] reversed = noSpaces.split(":");
-
-
-        for (int i = 0; i < reversed.length / 2; i++) {
-            String temp = reversed[i];
-            reversed[i] = reversed[reversed.length - i - 1];
-            reversed[reversed.length - i - 1] = temp;
-        }
-
-        return String.join(" ", reversed);
-    }
-
 
     public static void main(String[] args) {
+        char[] target;
+        target = new char[]{'h', 'e', 'l', 'l', 'o'};
+        reverseString(target);
+        Assert.assertEquals(target, "olleh".toCharArray());
 
-
-        String str = "a good   example";
-        Assert.assertEquals(reverseWords(str), "example good a");
-
-        Assert.assertEquals(reverseString("hello".toCharArray()), "olleh".toCharArray());
+        target = new char[]{'H', 'a', 'n', 'n', 'a', 'h'};
+        reverseString(target);
+        Assert.assertEquals(target, "hannaH".toCharArray());
     }
 
 }
