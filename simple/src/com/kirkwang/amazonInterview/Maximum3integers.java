815f1e7f0db949c0e120c783c47b47ad5acca4bd
kirkwang <kirkwangke@gmail.com>
2024-08-02 15:53:27 -0700

    update maximum3Integers

--- simple/src/com/kirkwang/amazonInterview/Maximum3integers.java
+++ simple/src/com/kirkwang/amazonInterview/Maximum3integers.java
@@ -6,31 +6,46 @@
 
 package com.kirkwang.amazonInterview;
 
+import org.testng.Assert;
+
 import java.util.Arrays;
 
+/*
+https://leetcode.com/problems/maximum-product-of-three-numbers/description/
+Example 1:
+
+Input: nums = [1,2,3]
+Output: 6
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: 24
+Example 3:
+
+Input: nums = [-1,-2,-3]
+Output: -6
+
+ */
 public class Maximum3integers {
-    public static int maximum3Integers(int[] input) {
+    public static int maximum3Integers(int[] nums) {
         int result = 0;
-        if (input.length < 3) {
+        if (nums.length < 3) {
             return result;
         }
+        Arrays.sort(nums);
+        int case1 = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
+        int case2 = nums[0] * nums[1] * nums[nums.length - 1];
+        return Math.max(case1, case2);
 
-        Arrays.sort(input);
 
-        if (input[0] <= 0) {
-            return input[input.length - 1] * input[input.length - 2] * input[input.length - 3];
-        }
-        if (input[0] < 0 && input[1] < 0) {
-            return input[input.length - 1] * input[0] * input[1];
-        }
-
-        return result;
     }
 
     public static void main(String[] args) {
         int[] array = new int[]{10, 67, 28, 0, -1, -30, -20};
-        int result = Maximum3integers.maximum3Integers(array);
 
-        System.out.println(result);
+        Assert.assertEquals(Maximum3integers.maximum3Integers(array), 40200);
+        Assert.assertEquals(Maximum3integers.maximum3Integers(new int[]{-1, -2, -3}), -6);
+        Assert.assertEquals(Maximum3integers.maximum3Integers(new int[]{1, 2, 3, 4}), 24);
+
     }
 }
