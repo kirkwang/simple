b8e54fcd7e644cb6b3bb86664878004af48fe52b
kirkwang <kirkwangke@gmail.com>
2024-09-04 15:29:10 -0700

    update Minimum_Absolute_Difference

--- simple/src/com/leetcode/easy/Minimum_Absolute_Difference.java
+++ simple/src/com/leetcode/easy/Minimum_Absolute_Difference.java
@@ -4,20 +4,21 @@ package com.leetcode.easy;
 https://leetcode.com/problems/minimum-absolute-difference/
 
 Example 1:
-
 Input: arr = [4,2,1,3]
 Output: [[1,2],[2,3],[3,4]]
 Explanation: The minimum absolute difference is 1. List all pairs with difference equal to 1 in ascending order.
-Example 2:
 
+Example 2:
 Input: arr = [1,3,6,10,15]
 Output: [[1,3]]
-Example 3:
 
+Example 3:
 Input: arr = [3,8,-10,23,19,-4,-14,27]
 Output: [[-14,-10],[19,23],[23,27]]
 
 */
+
+import org.testng.Assert;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -26,7 +27,7 @@ import java.util.List;
 public class Minimum_Absolute_Difference {
 
     public static List<List<Integer>> minimumAbsDifference(int[] arr) {
-        List result = new ArrayList();
+        List<List<Integer>> result = new ArrayList<>();
         int min_diff = Integer.MAX_VALUE;
         Arrays.sort(arr);
         for (int i = 0; i < arr.length - 1; i++) {
@@ -34,10 +35,10 @@ public class Minimum_Absolute_Difference {
                 min_diff = arr[i + 1] - arr[i];
             }
         }
-        List temp = null;
+        List<Integer> temp;
         for (int i = 0; i < arr.length - 1; i++) {
             if ((arr[i + 1] - arr[i]) == min_diff) {
-                temp = new ArrayList<Integer>();
+                temp = new ArrayList<>();
 
                 temp.add(arr[i]);
                 temp.add(arr[i + 1]);
@@ -50,10 +51,21 @@ public class Minimum_Absolute_Difference {
 
     public static void main(String[] args) {
 
+        List<List<Integer>> actual = minimumAbsDifference(new int[]{4, 2, 1, 3});
+        List<int[]> expectedList = new ArrayList<>();
+        expectedList.add(new int[]{1, 2});
+        expectedList.add(new int[]{2, 3});
+        expectedList.add(new int[]{3, 4});
 
-        //   int[] input = new int[]{4, 2, 1, 3};
-        //   int[] input = new int[]{1,3,6,10,15};
-        int[] input = new int[]{3, 8, -10, 23, 19, -4, -14, 27};
-        System.out.println(minimumAbsDifference(input));
+        actual.retainAll(expectedList);
+        Assert.assertEquals(actual.size(), 0);
+
+
+        actual = minimumAbsDifference(new int[]{1, 3, 6, 10, 15});
+        expectedList.clear();
+
+        actual.retainAll(expectedList);
+        Assert.assertEquals(actual.size(), 0);
+
     }
 }
