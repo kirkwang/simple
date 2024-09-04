11ca7c55ef82cf677303b14c9b9eaaa848cd483c
kirkwang <kirkwangke@gmail.com>
2024-09-03 22:41:06 -0700

    update MergeTwoArraysIntoOne

--- simple/src/com/leetcode/easy/MergeTwoArraysIntoOne.java
+++ simple/src/com/leetcode/easy/MergeTwoArraysIntoOne.java
@@ -8,7 +8,22 @@ import org.testng.Assert;
 /*
 https://leetcode.com/problems/merge-sorted-array/description/
 Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+Example 1:
 
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+Example 2:
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+Example 3:
+
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
 Note:
 You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
 
