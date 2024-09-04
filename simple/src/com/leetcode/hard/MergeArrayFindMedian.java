cf25cf7673482b039983d07d72eb5230ea550bcf
kirkwang <kirkwangke@gmail.com>
2024-09-03 22:04:00 -0700

    update MergeArrayFindMedian

--- simple/src/com/leetcode/hard/MergeArrayFindMedian.java
+++ simple/src/com/leetcode/hard/MergeArrayFindMedian.java
@@ -1,8 +1,10 @@
-package com.leetcode.easy;
+package com.leetcode.hard;
 
 
 
-/* Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median
+/*
+https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median
 of the two sorted arrays.
 
 Example 1:
@@ -16,6 +18,8 @@ Output: 2.50000
 Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 */
 
+import org.testng.Assert;
+
 import java.util.Arrays;
 
 class MergeArrayFindMedian {
@@ -25,26 +29,25 @@ class MergeArrayFindMedian {
         int[] two = new int[]{3};
 
         MergeArrayFindMedian mc = new MergeArrayFindMedian();
-        for (int i : mc.mergTwoArray(one, two)) {
-            System.out.println(i);
-        }
 
         int[] meraged = mc.mergTwoArray(one, two);
-        double result = mc.findMedian(meraged);
-        System.out.println("average " + result);
+        Assert.assertEquals(mc.findMedian(meraged), 2.00000);
+
+        meraged = mc.mergTwoArray(new int[]{1, 2}, new int[]{3, 4});
+        Assert.assertEquals(mc.findMedian(meraged), 2.50000);
+
 
     }
 
     public int[] mergTwoArray(int[] one, int[] two) {
         int[] mergedArray = new int[one.length + two.length];
         int index = 0;
-        for (int i = 0; i < one.length; i++) {
-            mergedArray[i] = one[i];
-            index++;
-        }
 
-        for (int i = 0; i < two.length; i++) {
-            mergedArray[index++] = two[i];
+        for (int i : one) {
+            mergedArray[index++] = i;
+        }
+        for (int j : two) {
+            mergedArray[index++] = j;
         }
 
         Arrays.sort(mergedArray);
@@ -55,10 +58,10 @@ class MergeArrayFindMedian {
     public double findMedian(int[] input) {
         //Explanation: merged array = [1,2,3] and median is 2.
 
-        double answer = 0.0;
+        double answer;
         if (input.length % 2 != 0) {
             //if it is odd length then get middle element
-            answer = (float) input[(input.length - 1) / 2];
+            answer = input[(input.length - 1) / 2];
         } else {
             int right = input[input.length / 2]; // [1,2,3,4] right = 3
             int left = input[(input.length - 1) / 2]; // [1,2,3,4] left = 2
