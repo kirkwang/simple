3a2bdf2790e9c7c9ed0872c13ae9b47f9c4b1ed4
kirkwang <kirkwangke@gmail.com>
2024-09-04 22:40:48 -0700

    update PalindromeNumber

--- simple/src/com/leetcode/easy/PalindromeNumber.java
+++ simple/src/com/leetcode/easy/PalindromeNumber.java
@@ -1,13 +1,34 @@
 package com.leetcode.easy;
 
+import org.testng.Assert;
+
 /*
 https://leetcode.com/problems/palindrome-number/
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
  */
 public class PalindromeNumber {
     public static void main(String[] args) {
         PalindromeNumber pn = new PalindromeNumber();
-        int input = 101;
-        System.out.println(pn.isPalindrome(input));
+
+        Assert.assertTrue(pn.isPalindrome(101));
+        Assert.assertTrue(pn.isPalindrome(121));
+        Assert.assertFalse(pn.isPalindrome(-121));
+        Assert.assertFalse(pn.isPalindrome(10));
     }
 
     public boolean isPalindrome(int x) {
