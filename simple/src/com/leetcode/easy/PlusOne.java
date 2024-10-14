e854b1b5f37e7624c956874dd7a2a9130a8e190c
kirkwang <kirkwangke@gmail.com>
2024-10-14 13:09:52 -0700

    update PlusOne

--- simple/src/com/leetcode/easy/PlusOne.java
+++ simple/src/com/leetcode/easy/PlusOne.java
@@ -26,9 +26,10 @@ Thus, the result should be [1,0].
 public class PlusOne {
     public static void main(String[] args) {
 
-        assertArrayEquals(PlusOne.plusOne(new int[]{9}), new int[]{1, 0});
-        assertArrayEquals(PlusOne.plusOne(new int[]{1, 2, 3}), new int[]{1, 2, 4});
-        assertArrayEquals(PlusOne.plusOne(new int[]{4, 3, 2, 1}), new int[]{4, 3, 2, 2});
+        assertArrayEquals(new int[]{1, 0}, PlusOne.plusOne(new int[]{9}));
+        assertArrayEquals(new int[]{1, 2, 4}, PlusOne.plusOne(new int[]{1, 2, 3}));
+        assertArrayEquals(new int[]{4, 3, 2, 3}, PlusOne.plusOne(new int[]{4, 3, 2, 2}));
+        assertArrayEquals(new int[]{9, 1, 0,}, PlusOne.plusOne(new int[]{9, 0, 9}));
 
     }
 
