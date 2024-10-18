8bf99067a8da2168628533782ed14e58e5dc84c3
kirkwang <kirkwangke@gmail.com>
2024-10-18 11:25:48 -0700

    update WeightedUniformStrings

--- simple/src/com/kirkwang/HackerRank/generalProgramming/WeightedUniformStrings.java
+++ simple/src/com/kirkwang/HackerRank/generalProgramming/WeightedUniformStrings.java
@@ -1,8 +1,9 @@
 package com.kirkwang.HackerRank.generalProgramming;
 
 
-import java.util.HashSet;
-import java.util.Set;
+import org.testng.Assert;
+
+import java.util.*;
 
 /* Traverse the string just 1 time. Save the weights of every possible uniform substring in a HashSet.
 https://www.hackerrank.com/challenges/weighted-uniform-string/problem
@@ -11,12 +12,33 @@ https://www.hackerrank.com/challenges/weighted-uniform-string/problem
 */
 public class WeightedUniformStrings {
     public static void main(String[] args) {
-        String str = "aaabbbbcccddd";
-        int[] querys = {9, 7, 8, 12, 5};
-        Set<Integer> weights = getWeights(str);
-        for (int query : querys) {
+
+
+        List<Integer> querysList = new ArrayList<>();
+        querysList.add(9);
+        querysList.add(7);
+        querysList.add(8);
+        querysList.add(12);
+        querysList.add(5);
+
+        List<String> results = weightedUniformStrings("aaabbbbcccddd", querysList);
+
+        String[] expectedArray = {"Yes", "No", "Yes", "Yes", "No"};
+        List<String> expected = new ArrayList<>(Arrays.asList(expectedArray));
+
+        Assert.assertEquals(results, expected);
+
+    }
+
+    public static List<String> weightedUniformStrings(String s, List<Integer> queries) {
+
+        Set<Integer> weights = getWeights(s);
+        List<String> results = new ArrayList<>();
+        for (Integer query : queries) {
+            results.add(weights.contains(query) ? "Yes" : "No");
             System.out.println(weights.contains(query) ? "Yes" : "No");
         }
+        return results;
     }
 
     private static Set<Integer> getWeights(String str) {
