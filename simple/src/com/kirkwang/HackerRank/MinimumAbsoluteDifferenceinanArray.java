1274c3138297e6dab1ba1590945e074c18c4ca4a
kirkwang <kirkwangke@gmail.com>
2024-10-18 12:28:07 -0700

    update MinimumAbsoluteDifferenceinanArray

--- simple/src/com/kirkwang/HackerRank/MinimumAbsoluteDifferenceinanArray.java
+++ simple/src/com/kirkwang/HackerRank/MinimumAbsoluteDifferenceinanArray.java
@@ -6,27 +6,27 @@
 
 package com.kirkwang.HackerRank;
 
-import java.util.ArrayList;
-import java.util.Comparator;
+import org.testng.Assert;
+
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-//https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
+/*
+https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
+ */
 public class MinimumAbsoluteDifferenceinanArray {
     public static void main(String[] args) {
 
-        // int[] array = {-59, -36, -13, 1, -53, -92, -2, -96, -54, 75};
-        int[] array = {1, -3, 71, 68, 17};
-
-        List<Integer> arrayList = new ArrayList<>();
-        for (int i : array) {
-            arrayList.add(i);
-        }
-        System.out.println(minimumAbsoluteDifference(arrayList));
+        Assert.assertEquals(minimumAbsoluteDifference(Arrays.asList(1, -3, 71, 68, 17)), 3);
+        Assert.assertEquals(minimumAbsoluteDifference(Arrays.asList(3, -7, 0)), 3);
+        Assert.assertEquals(minimumAbsoluteDifference(Arrays.asList(-59, -36, -13, 1, -53, -92, -2, -96, -54, 75)), 1);
 
     }
 
     public static int minimumAbsoluteDifference(List<Integer> arr) {
-        arr.sort(Comparator.naturalOrder());
+
+        Collections.sort(arr);
         int min = Integer.MAX_VALUE;
 
         for (int i = 1; i < arr.size(); i++) {
