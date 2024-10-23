5b6f116cd14d860f0170daaa32eb0d788a30698f
kirkwang <kirkwangke@gmail.com>
2024-10-23 15:32:18 -0700

    update EvaluateValue

--- simple/src/com/leetcode/easy/EvaluateValue.java
+++ simple/src/com/leetcode/easy/EvaluateValue.java
@@ -13,27 +13,30 @@ import java.util.Stack;
 
 public class EvaluateValue {
 
-
     public static void main(String[] args) {
-        String[] tokens = new String[]{"2", "1", "+"};
 
-        Assert.assertEquals(evaluate(tokens), 3);
+        Assert.assertEquals(evaluate(new String[]{"2", "1", "+"}), 3);
+        Assert.assertEquals(evaluate(new String[]{"2", "3", "+"}), 5);
+        Assert.assertEquals(evaluate(new String[]{"2", "3", "-"}), -1);
+        Assert.assertEquals(evaluate(new String[]{"3", "2", "-"}), 1);
+        Assert.assertEquals(evaluate(new String[]{"3", "2", "*"}), 6);
+        Assert.assertEquals(evaluate(new String[]{"3", "2", "/"}), 1);
     }
 
-    public static int evaluate(String[] token) {
+    public static int evaluate(String[] expression) {
         Stack<String> stack = new Stack();
         String operator = "+-*/";
 
-        for (String t : token) {
-            //if it's not operator or it's a number, push to stack
-            if (!operator.contains(t)) {
-                stack.push(t);
+        for (String s : expression) {
+            //if it's not operator, push the number to stack
+            if (!operator.contains(s)) {
+                stack.push(s);
             } else {
                 // when there is operator, pop two of them
                 int x = Integer.valueOf(stack.pop());
                 int y = Integer.valueOf(stack.pop());
 
-                int index = operator.indexOf(t);
+                int index = operator.indexOf(s);
                 switch (index) {
                     case 0: {
                         stack.push(String.valueOf(x + y));
