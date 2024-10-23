ddc1f9593a40824482afe0a2568d75b26a387864
kirkwang <kirkwangke@gmail.com>
2024-10-23 11:35:59 -0700

    update CountCharacters

--- simple/src/com/leetcode/easy/CountCharacters.java
+++ simple/src/com/leetcode/easy/CountCharacters.java
@@ -39,37 +39,13 @@ public class CountCharacters {
                     tempMap[s.charAt(i) - 'a']--;
                 }
             }
-            if (flag) result = result + s.length();
+            if (flag) {
+                result = result + s.length();
+            }
         }
-
-
         return result;
     }
 
-    public static int countCharacters_leetcode(String[] words, String chars) {
-        int[] map = new int[26];
-        int res = 0;
-
-        for (int i = 0; i < chars.length(); i++) {
-            map[chars.charAt(i) - 'a']++;
-        }
-
-        for (String word : words) {
-            int[] tempMap = map.clone();
-            boolean flag = true;
-            for (int i = 0; i < word.length(); i++) {
-                if (tempMap[word.charAt(i) - 'a'] == 0) {
-                    flag = false;
-                    break;
-                } else {
-                    tempMap[word.charAt(i) - 'a']--;
-                }
-            }
-            if (flag) res += word.length();
-        }
-
-        return res;
-    }
 
     public static void main(String[] args) {
         // some of the test cases are not working.
