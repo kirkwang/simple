bb102c5ef30323bf997f8fe55164a877c38493f4
kirkwang <kirkwangke@gmail.com>
2024-10-25 16:38:42 -0700

    updating LongestPalindromicSubstring

--- simple/src/com/leetcode/easy/LongestPalindromicSubstring.java
+++ simple/src/com/leetcode/easy/LongestPalindromicSubstring.java
@@ -1,11 +1,11 @@
 package com.leetcode.easy;
 
 import org.testng.Assert;
-
+/*
+https://leetcode.com/problems/longest-palindromic-substring/description/
+ */
 /**
  * Created by kewang on 12/12/17.
- *
- * <p><a href="https://leetcode.com/problems/longest-palindromic-substring/description/">...</a>
  *
  * <p>Input: "babad"
  *
@@ -19,7 +19,7 @@ import org.testng.Assert;
  */
 public class LongestPalindromicSubstring {
 
-    public static String LongestPalindromicSubstring(String input) {
+    public static String longestPalindromicSubstring(String input) {
         int tail = 0;
         int head = 0;
 
@@ -33,7 +33,7 @@ public class LongestPalindromicSubstring {
         for (int i = 0; i < input.length() - 1; i++) {
             int len1 = checkPalindromicSubstring(input, i, i);
             int len2 = checkPalindromicSubstring(input, i, i + 1);
-            int len = Math.max(len1, len2);
+            int len = Math.max(len1, len2); // matcher (aa || aba) length
             if (len > tail - head) {
                 head = i - (len - 1) / 2; // head is shift left(-) size /2
                 tail = i + len / 2;  // tail is i shift right(+) size/2
@@ -43,32 +43,24 @@ public class LongestPalindromicSubstring {
         return input.substring(head, tail + 1);
     }
 
-    private static int findpalindromicsubstring(String input, int i, int j) {
-        while (i >= 0 && j < input.length() && input.charAt(i) == input.charAt(j)) {
-            i--;
-            j++;
-        }
-        return j - i - 1;
-    }
-
     private static int checkPalindromicSubstring(String target, int i, int j) {
         while (i >= 0 && j < target.length() && target.charAt(i) == target.charAt(j)) {
-            i--;
-            j++;
+            i--;  //look to the left
+            j++;  //look to the right
         }
         return j - i - 1;
     }
 
     public static void main(String[] args) {
 
-        String result = LongestPalindromicSubstring.LongestPalindromicSubstring("ababada");
+        String result = LongestPalindromicSubstring.longestPalindromicSubstring("ababada");
         Assert.assertEquals(result, "ababa");
 
-        String result1 = LongestPalindromicSubstring.LongestPalindromicSubstring("acbbcd");
+        String result1 = LongestPalindromicSubstring.longestPalindromicSubstring("acbbcd");
         Assert.assertEquals(result1, "cbbc");
 
-        Assert.assertEquals(LongestPalindromicSubstring.LongestPalindromicSubstring("babad"), "aba");
-        Assert.assertEquals(LongestPalindromicSubstring.LongestPalindromicSubstring("cbbd"), "bb");
+        Assert.assertEquals(LongestPalindromicSubstring.longestPalindromicSubstring("babad"), "aba");
+        Assert.assertEquals(LongestPalindromicSubstring.longestPalindromicSubstring("cbbd"), "bb");
 
     }
 }
