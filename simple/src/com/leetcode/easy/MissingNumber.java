bae56da4433bbf9199eb102fad1424fda8ce5d4f
kirkwang <kirkwangke@gmail.com>
2024-10-28 10:56:09 -0700

    updating MissingNumber

--- simple/src/com/leetcode/easy/MissingNumber.java
+++ simple/src/com/leetcode/easy/MissingNumber.java
@@ -24,18 +24,18 @@ import org.testng.Assert;
 public class MissingNumber {
     public static void main(String[] args) {
 
-        Assert.assertEquals(missingNumber(new int[]{0, 1, 3}), 2);
+        Assert.assertEquals(missingNumber(new int[]{3, 0, 1}), 2);
         Assert.assertEquals(missingNumber(new int[]{0, 1}), 2);
         Assert.assertEquals(missingNumber(new int[]{9, 6, 4, 2, 3, 5, 7, 0, 1}), 8);
     }
 
     public static int missingNumber(int[] nums) {
-        int expectedSum = nums.length * (nums.length + 1) / 2;  // (3 * 4) /2 = 6 , 6 - 1 -3 = 2
+        int total = nums.length * (nums.length + 1) / 2;  // (3 * 4) /2 = 6 , 6 - 1 -3 = 2
         int actualSum = 0;
         for (int i : nums) {
             actualSum = actualSum + i;
         }
 
-        return expectedSum - actualSum;
+        return total - actualSum;
     }
 }
