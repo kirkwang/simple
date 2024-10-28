6af8e1de91eb9158254eaaf7465d210c852364c0
kirkwang <kirkwangke@gmail.com>
2024-10-28 13:26:02 -0700

    updating productOfArrayExceptSelf

--- simple/src/com/leetcode/easy/ProductOfArrayExceptSelf.java
+++ simple/src/com/leetcode/easy/ProductOfArrayExceptSelf.java
@@ -15,41 +15,42 @@ Given an integer array nums, return an array answer such that answer[i] is equal
     Output: [0,0,9,0,0]
 */
 
+import org.testng.Assert;
+
+import java.util.Arrays;
+
 public class ProductOfArrayExceptSelf {
 
-    public static int[] ProductOfArrayExceptSelf(int[] nums) {
-        int N = nums.length;
-        int[] result = new int[N];
-        int[] left = new int[N];
-        int[] right = new int[N];
+    public static int[] productOfArrayExceptSelf(int[] nums) {
+        int L = nums.length;
+        int[] result = new int[L];
+        int[] left = new int[L];
+        int[] right = new int[L];
         left[0] = 1;
-        right[N - 1] = 1;
+        right[L - 1] = 1;
 //  1,2,3,4
 //  1,1,2,6
-        for (int i = 1; i < N; i++) {
+        for (int i = 1; i < L; i++) {
             left[i] = nums[i - 1] * left[i - 1];
-
         }
         // 1,2,3,4
         // 24,12,4, 1
-        for (int i = N - 2; i >= 0; i--) {
+        for (int i = L - 2; i >= 0; i--) {
             right[i] = nums[i + 1] * right[i + 1];
 
         }
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < L; i++) {
             result[i] = left[i] * right[i];
 
         }
-
         return result;
     }
 
-    public static void main(java.lang.String args[]) {
+    public static void main(String... args) {
 
         int[] input = new int[]{1, 2, 3, 4};
-        for (int i : ProductOfArrayExceptSelf(input)) {
-            System.out.println(i);
-        }
+
+        Assert.assertTrue(Arrays.equals(productOfArrayExceptSelf(input), new int[]{24, 12, 8, 6}));
     }
 }
