6f8374108a36dd8587463f9aa490253af786b8cb
kirkwang <kirkwangke@gmail.com>
2024-12-06 11:12:10 -0800

    Adding PrintLinkedList

---
+++ simple/src/com/kirkwang/hackerrank/generalprogramming/PrintLinkedList.java
@@ -0,0 +1,49 @@
+package com.kirkwang.hackerrank.generalprogramming;
+
+public class PrintLinkedList {
+    static void printLinkedList(SinglyLinkedListNode head) {
+        while (head != null) {
+            System.out.println(head.data);
+            head = head.next;
+        }
+    }
+
+    public static void main(String... args) {
+        SinglyLinkedListNode head = new SinglyLinkedListNode(16);
+
+        head.next = new SinglyLinkedListNode(13);
+        printLinkedList(head);
+    }
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+}
